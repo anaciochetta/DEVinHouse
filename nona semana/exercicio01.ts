53048ba126fb7537c62d3017b0fef4c0fb9b7a8b
aciochetta <anaciochetta@gmail.com>
2022-05-03 16:39:57 -0300

    03/05 - aula 25/04 + exercicios 1 e 2

---
+++ nona semana/exercicio01.ts
@@ -0,0 +1,3 @@
+let idade: number = 22;
+const nome: string = "ana";
+var resposta: any = "Olá, meu nome é " + nome + " e tenho " + idade + " anos.";
---
+++ nona semana/exercicio02.ts
@@ -0,0 +1,3 @@
+const funcao = (a: 2, b: 6): number => {
+  return a + b;
+};
