c288127cdc7d4a265103e372445847989954c987
aciochetta <anaciochetta@gmail.com>
2022-05-20 15:20:17 -0300

    20/05

--- décima primeira semana/semana-onze/src/app/app.module.ts
+++ décima primeira semana/semana-onze/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentesComponent } from './componentes/componentes.component';
+import { ExercicioUmComponent } from './componentes/exercicio-um/exercicio-um.component';
 
 @NgModule({
-  declarations: [AppComponent, ComponentesComponent],
+  declarations: [AppComponent, ComponentesComponent, ExercicioUmComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ décima primeira semana/semana-onze/src/app/componentes/exercicio-um/exercicio-um.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'smo-exercicio-um',
+  templateUrl: './exercicio-um.component.html',
+  styleUrls: ['./exercicio-um.component.scss']
+})
+export class ExercicioUmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
