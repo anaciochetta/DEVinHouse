449302565f660e6fe4ce8ccbd087e05ec897593a
aciochetta <anaciochetta@gmail.com>
2022-09-15 20:02:49 -0300

    exercício 09 - semana 03
    Criação de DTO e controller de Autenticação

---
+++ ModuloTres/RH/Controller/AutenticacaoController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RH.DTOs;
+using RH.Repositories;
+using RH.Services;
+
+namespace RH.Controller
+{
+    public class AutenticacaoController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login([FromBody] FuncionarioDTO dto)
+        {
+
+            var user = FuncionarioRepository.GetByLoginAndPassword(dto.Email, dto.Senha);
+
+            if (user == null) return Unauthorized();
+
+            var token = TokenService.GenerateToken(user);
+
+            return Ok(new { token });
+        }
+    }
+}
---
+++ ModuloTres/RH/DTOs/FuncionarioDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RH.DTOs
+{
+    public class FuncionarioDTO
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
